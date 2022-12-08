941ec0f2321f8559a711e7adfeb11c6c3c002cc5
albanlandry <malbanjunior@yahoo.fr>
2022-12-08 18:12:32 +0900

    shape integration

--- pages/src/drawing/Canvas.js
+++ pages/src/drawing/Canvas.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Victor from 'victor';
 import PropTypes from 'prop-types';
 
@@ -14,11 +14,37 @@ const Canvas = (props) => {
     const canvasWidth = props.width || CANVAS_DEFAULT_WIDTH;
     const canvasHeight = props.height || CANVAS_DEFAULT_HEIGHT;
     const canvasRef = useRef(null);
+    const [components, setComponents] = useState(null);
+    const animationId = useRef(null);
+
+    useEffect(() => {
+        const cvs = canvasRef.current;
+
+        // Add the context property to each child component.
+        // Since the react components are sealed object, we cannot directly edit their properties.
+        // Thus, we need to clone the object with the additional property
+        const res = Array(props.children).flat().map((child, id) =>  React.cloneElement(child, {context: cvs.getContext('2d'), key: id}) );
+
+        setComponents(res)
+
+        const render = () => {
+            
+
+            animationId.current = window.requestAnimationFrame(render);
+        };
+
+        return() => {
+            window.cancelAnimationFrame(animationId.current);
+        }
+    }, [])
+    // Adding the canvas property to the children
 
     return (
         <canvas
-            className = "border border-solid border-black"
-            ref={canvasRef} width={canvasWidth} height={canvasHeight}></canvas>
+            className = "border border-solid border-black box-border"
+            ref={canvasRef} width={canvasWidth} height={canvasHeight}>
+            {components}
+        </canvas>
     )
 };
 
